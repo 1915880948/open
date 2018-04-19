4307d73cbdb799e4a345a86928c335b31003b801
Jun <394383890@qq.com>
2018-04-20 01:00:29 +0800

    url manager

--- controllers/OauthController.php
+++ controllers/OauthController.php
@@ -15,9 +15,8 @@ use yii\web\Controller;
 class OauthController extends Controller {
     public function actionCallback($state){
         $code = Yii::$app->request->get('code');
-        var_dump(base64_decode($state));
-//        $state = Yii::$app->request->get('state');
-//        return $this->redirect($state . '?code=' . $code);
+        $redirect = base64_decode($state);
+        return $this->redirect($redirect . '?code=' . $code);
     }
 
     public function actionIndex(){

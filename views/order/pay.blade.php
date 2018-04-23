514e2c66d468f67c7ae56c00fb5b60b659ccb405
Jun <394383890@qq.com>
2018-04-23 14:53:24 +0800

    支付

--- views/order/pay.blade.php
+++ views/order/pay.blade.php
@@ -7,15 +7,18 @@
 @section('foot-script')
     <script type='text/javascript' src='http://res.wx.qq.com/open/js/jweixin-1.3.0.js'></script>
     <script>
-        wx.chooseWXPay({
-            timestamp: '{{ $config['timestamp'] }}',
-            nonceStr: '{{ $config['nonceStr'] }}',
-            package: '{{ $config['package'] }}',
-            signType: '{{ $config['signType'] }}',
-            paySign: '{{ $config['paySign'] }}', // 支付签名
-            success: function (res) {
-                alert('支付成功');
-            }
-        });
+        function pay() {
+            wx.chooseWXPay({
+                timestamp: '{{ $config['timestamp'] }}',
+                nonceStr: '{{ $config['nonceStr'] }}',
+                package: '{{ $config['package'] }}',
+                signType: '{{ $config['signType'] }}',
+                paySign: '{{ $config['paySign'] }}', // 支付签名
+                success: function (res) {
+                    alert('支付成功');
+                }
+            });
+        }
+        pay();
     </script>
 @stop

a39c5d3fc124b31232c8e87b0f30617893de1bec
Jun <394383890@qq.com>
2018-04-23 14:54:41 +0800

    支付

--- views/order/pay.blade.php
+++ views/order/pay.blade.php
@@ -8,6 +8,7 @@
     <script type='text/javascript' src='http://res.wx.qq.com/open/js/jweixin-1.3.0.js'></script>
     <script>
         function pay() {
+            console.log('aaa');
             wx.chooseWXPay({
                 timestamp: '{{ $config['timestamp'] }}',
                 nonceStr: '{{ $config['nonceStr'] }}',

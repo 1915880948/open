3a3bfbef70b6d86667934d8c05deecc3e44ce4fe
Jun <394383890@qq.com>
2018-04-23 15:02:22 +0800

    支付

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -9,6 +9,15 @@
 </head>
 <body>
 @yield('content')
+<script type="text/javascript" charset="utf-8">
+    $(function () {
+        {{--wx.config({!! jssdk()->setUrl(yUrlCurrent())->config(['checkJsApi','onMenuShareTimeline','onMenuShareAppMessage',--}}
+        {{--'menuItem:profile','menuItem:addContact','menuItem:share:appMessage','menuItem:share:timeline']) !!});--}}
+        wx.config({!! \app\common\wechat\Weixin::getApp()->js->config(['checkJsApi','onMenuShareTimeline','onMenuShareAppMessage',
+                    'menuItem:profile','menuItem:addContact','menuItem:share:appMessage','menuItem:share:timeline','chooseWXPay',"chooseImage", "previewImage", "uploadImage", "downloadImage",
+                     'startRecord', 'stopRecord', 'onVoiceRecordEnd', 'playVoice', 'pauseVoice', 'uploadVoice']) !!});
+    })
+</script>
 @yield('foot-script')
 </body>
 </html>

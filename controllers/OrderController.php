c936462606c6805af027ad083f896d0e4b5db519
Jun <394383890@qq.com>
2018-04-23 13:58:53 +0800

    支付

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -21,44 +21,40 @@ use yii\helpers\Url;
 
 class OrderController extends AdminBaseController {
 
-    public function actionPay(){
+    public function actionPay() {
         $json_detail = Yii::$app->request->post('json_detail');
         $attributes = Json::decode($json_detail);
         $result = Weixin::getPayment()->order->unify($attributes);
         $config = [];
-//        var_dump($result['return_code']);die;
-        if($result['return_code'] === 'SUCCESS'){
-            try {
-                $prepayId = $result['prepay_id'];
-                $jssdk = Weixin::getPayment()->jssdk;
-                $config = $jssdk->sdkConfig($prepayId);
-//                $pay = "wx.chooseWXPay({
-//                        timestamp: {$config['timestamp']},
-//                        nonceStr: '{$config['nonceStr']}',
-//                        package: '{$config['package']}',
-//                        signType: '{$config['signType']}',
-//                        paySign: '{$config['paySign']}', // 支付签名
-//                        success: function (res) {
-//                            alert('支付成功');
-//                        }
-//                    });";
-//                echo "<script type='text/javascript' src='http://res.wx.qq.com/open/js/jweixin-1.3.0.js'></script>";
-//                echo "<script type='text/javascript'>{$pay}</script>";
-                return $this->render('pay', compact('config'));
-            } catch (Exception $e){
-                echo $e->getMessage();
-            }
+        //        var_dump($result['return_code']);die;
+        if ($result['return_code'] === 'SUCCESS') {
+            $prepayId = $result['prepay_id'];
+            $jssdk = Weixin::getPayment()->jssdk;
+            $config = $jssdk->sdkConfig($prepayId);
+            //                $pay = "wx.chooseWXPay({
+            //                        timestamp: {$config['timestamp']},
+            //                        nonceStr: '{$config['nonceStr']}',
+            //                        package: '{$config['package']}',
+            //                        signType: '{$config['signType']}',
+            //                        paySign: '{$config['paySign']}', // 支付签名
+            //                        success: function (res) {
+            //                            alert('支付成功');
+            //                        }
+            //                    });";
+            //                echo "<script type='text/javascript' src='http://res.wx.qq.com/open/js/jweixin-1.3.0.js'></script>";
+            //                echo "<script type='text/javascript'>{$pay}</script>";
+            return $this->render('pay', compact('config'));
         } else {
             echo 'Error';
         }
 
     }
 
-    public function actionNotify(){
+    public function actionNotify() {
         $payment = Weixin::getPayment();
-        $response = $payment->handlePaidNotify(function ($message, $fail){
+        $response = $payment->handlePaidNotify(function ($message, $fail) {
             $order = GuaguaOrder::find()->andWhere(['out_trade_no' => $message['out_trade_no']])->one();
-            if(!$order || $order->paid_at){
+            if (!$order || $order->paid_at) {
                 return true;
             }
             if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
@@ -69,7 +65,7 @@ class OrderController extends AdminBaseController {
                     $order->order_status = 1;
                     // 支付成功，第一次购买，在user表中增加一条数据。
                     $account = Account::find()->andWhere(['id' => $order->uid])->asArray()->one();
-                    if ( !$account['uid'] ) {
+                    if (!$account['uid']) {
                         $info = [
                             'account_id' => $order->uid,
                             'username' => $order->contact_name,

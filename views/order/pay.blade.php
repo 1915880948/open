05f7c822c981560ba22ea448850fd556c28dc413
Jun <394383890@qq.com>
2018-04-23 15:15:17 +0800

    支付

--- views/order/pay.blade.php
+++ views/order/pay.blade.php
@@ -7,19 +7,21 @@
 
 @section('foot-script')
     <script>
-        function pay() {
-            console.log('aaa');
-            wx.chooseWXPay({
-                timestamp: '{{ $config['timestamp'] }}',
-                nonceStr: '{{ $config['nonceStr'] }}',
-                package: '{{ $config['package'] }}',
-                signType: '{{ $config['signType'] }}',
-                paySign: '{{ $config['paySign'] }}', // 支付签名
-                success: function (res) {
-                    alert('支付成功');
-                }
-            });
-        }
-        pay();
+        $(function () {
+            function pay() {
+                console.log('aaa');
+                wx.chooseWXPay({
+                    timestamp: '{{ $config['timestamp'] }}',
+                    nonceStr: '{{ $config['nonceStr'] }}',
+                    package: '{{ $config['package'] }}',
+                    signType: '{{ $config['signType'] }}',
+                    paySign: '{{ $config['paySign'] }}', // 支付签名
+                    success: function (res) {
+                        alert('支付成功');
+                    }
+                });
+            }
+            pay();
+        });
     </script>
 @stop

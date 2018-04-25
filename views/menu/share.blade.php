127bb51dd51a52b824cd450850f0582f707f4b35
Jun <394383890@qq.com>
2018-04-26 01:34:57 +0800

    分享

--- views/menu/share.blade.php
+++ views/menu/share.blade.php
@@ -12,15 +12,18 @@
 @section('foot-script')
 <script>
     $(function(){
-        wx.onMenuShareAppMessage({
-            title: '呱谷课堂', // 分享标题
-            desc: '呱谷课堂-首页', // 分享描述
-            link: 'http://open.guaschool.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://p5tjno6bs.bkt.clouddn.com/Fvk1hj9lDBlML77RhAY-cwfShqa6', // 分享图标
-            success: function () {
-                history.go(-1);
-            }
-        });
+        function share() {
+            wx.onMenuShareAppMessage({
+                title: '呱谷课堂', // 分享标题
+                desc: '呱谷课堂-首页', // 分享描述
+                link: 'http://open.guaschool.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://p5tjno6bs.bkt.clouddn.com/Fvk1hj9lDBlML77RhAY-cwfShqa6', // 分享图标
+                success: function () {
+                    history.go(-1);
+                }
+            });
+        }
+        share();
     });
 </script>
 @stop

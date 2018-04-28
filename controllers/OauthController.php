b95fe25439a909a62db99757ec41315a2d16534d
Jun <394383890@qq.com>
2018-04-28 13:51:19 +0800

    分享

--- controllers/OauthController.php
+++ controllers/OauthController.php
@@ -16,7 +16,8 @@ class OauthController extends Controller {
     public function actionCallback($state){
         $code = Yii::$app->request->get('code');
         $redirect = base64_decode($state);
-        var_dump($redirect);die;
+        $urls = parse_url($redirect);
+        var_dump($urls);die;
         $urls = explode('?', $redirect);
         if(isset($urls[1])){
             return $this->redirect($urls[0].'/oauth/callback?'. $urls[1].'&code='. $code);

fbddbc96720dafa23491dfa6b05e117876ef2503
dai <1915880948@qq.com>
2018-06-25 10:44:53 +0800

    Weixin_Payment_Key

--- config/params.php
+++ config/params.php
@@ -4,9 +4,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'Weixin' => [
         'Weixin_App_ID'=>"wx214c4534722c09e9",
-        'Weixin_App_Secret'=>"lzhQQRjJZXhVxGpItSymssAdfJdaCoZV",
-//        'Weixin_App_Secret'=>"f8d970a6392f27e0335d5dfe27bd0cef",
+        'Weixin_App_Secret'=>"f8d970a6392f27e0335d5dfe27bd0cef",
         'Weixin_Payment'=>'1501124981',
-        'Weixin_Payment_Key' =>'f8f348b5b3099ed7ef2064a53bb7c143'
+//        'Weixin_Payment_Key' =>'f8f348b5b3099ed7ef2064a53bb7c143'
+        'Weixin_Payment_Key' =>'lzhQQRjJZXhVxGpItSymssAdfJdaCoZV'
     ],
 ];
